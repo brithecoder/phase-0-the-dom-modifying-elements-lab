ab3b7ba540d60c96e1f43c8591593f7212ce27f1
Brianna Gordon <brigordon574@gmail.com>
2021-05-09 13:21:04 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+  document.querySelector("#main").remove();
+
+  const newHeader = document.createElement(`h1`);
+     
+ newHeader.id=`victory`;
+
+ newHeader.innerHTML="Brianna is the champion.";
+
